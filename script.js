ec7bdd7b8a46563844f31ae62f58d5bf79287d79
Thiago Mauricio Borges <tmb050703@gmail.com>
2025-02-21 15:54:24 -0300

    game rule

--- script.js
+++ script.js
@@ -101,5 +101,36 @@ btnHold.addEventListener('click', function(){
   }
 
 });
-
 btnNew.addEventListener('click', init);
+
+
+
+
+// modal
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for(let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+
+  
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+};
+  
+document.addEventListener('keydown', function(e) {
+
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
